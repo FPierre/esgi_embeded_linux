83f5f2492756064824ca7b19a2a0784bd71b451e
Save92 <njournaud@hotmail.fr>
2016-04-15 00:00:46 +0200

    add js in html debug

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,30 +2,33 @@
 
 var theUrl = "https://api.mlab.com/api/1/databases/linuxem/collections/temp?apiKey=Wsr5fq8hXd3xtNV_ERnMRSDO02J2U1kG"
 
-var test;
+var data;
+var temperature;
+var dateinfo;
+var time;
 httpGetAsync(theUrl, function(str){
 	console.log("data", str);
     // If property names are known beforehand, you can also just do e.g.
     // alert(object.id + ',' + object.Title);
-}
-	var data = JSON.parse(str, function(k, v) {
+	data = JSON.parse(str, function(k, v) {
 		//console.log("k", k);
 		//console.log("v", v);
 		switch(k) {
 			case "temperature" :
-				var temperature = v;
+				temperature = v;
 				console.log("temp", temperature);
 				break;
 			case "date" :
-				var dateinfo = v;
+				dateinfo = v;
 				console.log("date", dateinfo);
 				break;
 			case "time" :
-				var time = v;
+				time = v;
 				console.log("time", time);
 				break;
-
+			}
 		}
+		console.log("data",data);
 	});
 	
 });
