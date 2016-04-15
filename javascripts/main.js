a768498600d8264ab3168e9ad6112a6f29c5cbb6
Save92 <njournaud@hotmail.fr>
2016-04-15 11:47:14 +0200

    call api

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,7 @@
 
 var theUrl = "https://api.mlab.com/api/1/databases/linuxem/collections/temperature?apiKey=Wsr5fq8hXd3xtNV_ERnMRSDO02J2U1kG"
 
-var data = [];
+var data;
 var temperature;
 var dateinfo;
 var time;
@@ -13,30 +13,39 @@ init();
 function init() {
 	httpGetAsync(theUrl, function(str){
 	console.log("str", str);
-	data = str;
-	console.log("data", data);
+	// data = str;
+	// console.log("data", data);
     // If property names are known beforehand, you can also just do e.g.
     // alert(object.id + ',' + object.Title);
-	JSON.parse(str, function(k, v) {
+
+
+data = JSON.parse(str);
+console.log("data", data);
+for (var i = 0; i<data.length; i++) {
+	console.log("data.temp", data.temperature);
+	console.log("data.date", data.date);
+	console.log("data.time", data.time);
+}
+	// JSON.parse(str, function(k, v) {
 		
-		console.log("k", k);
-		console.log("v", v);
+	// 	console.log("k", k);
+	// 	console.log("v", v);
 		
-		switch(k) {
-			case "temperature" :
-				temperature = v/1000;
-				console.log("temp", temperature);
-				break;
-			case "date" :
-				dateinfo = v;
-				console.log("date", dateinfo);
-				break;
-			case "time" :
-				time = v;
-				console.log("time", time);
-				break;
-			}
-	});
+	// 	switch(k) {
+	// 		case "temperature" :
+	// 			temperature = v/1000;
+	// 			console.log("temp", temperature);
+	// 			break;
+	// 		case "date" :
+	// 			dateinfo = v;
+	// 			console.log("date", dateinfo);
+	// 			break;
+	// 		case "time" :
+	// 			time = v;
+	// 			console.log("time", time);
+	// 			break;
+	// 		}
+	// });
 		BindingElements();
 		console.log("data", data);
 
